6c2182dd44061bd5083df68949ece7cc5639310c
Fer Guevara <kafera.g10@gmail.com>
2022-09-07 18:04:44 -0500

    Práctica 2
    Práctica 2. Prueba GET lista de alumnos
    07-09-22

---
+++ PruebaIntSol/src/java/pojo/Alumno.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojo;
+
+/**
+ *
+ * @author kfgf1
+ */
+public class Alumno {
+    private String matricula;
+    private String nombre;
+    private String apellidos;
+    private Integer edad;
+
+    public Alumno() {
+    }
+
+    public Alumno(String matricula, String nombre, String apellidos, Integer edad) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+}
--- PruebaIntSol/src/java/ws/AlumnosWS.java
+++ PruebaIntSol/src/java/ws/AlumnosWS.java
@@ -5,6 +5,8 @@
  */
 package ws;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -13,6 +15,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import pojo.Alumno;
 
 /**
  * REST Web Service
@@ -34,7 +37,6 @@ public class AlumnosWS {
     /**
      * Retrieves representation of an instance of ws.AlumnoWS
      * @return an instance of java.lang.String
-     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
@@ -45,9 +47,25 @@ public class AlumnosWS {
     /**
      * PUT method for updating or creating an instance of AlumnoWS
      * @param content representation for the resource
-     */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
+    }
+    */
+    
+    @Path("all") //ruta
+    @GET //tipo de petición
+    @Produces(MediaType.APPLICATION_JSON) //cómo se regresa(recuperar) la información
+    public List<Alumno> obtenerTodos(){
+        List<Alumno> alumnos = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            Alumno alum = new Alumno();
+            alum.setMatricula("s03298932"+i);
+            alum.setNombre("Prueba"+i);
+            alum.setApellidos("Apellidos"+i);
+            alum.setEdad(i);
+            alumnos.add(alum);
+        }
+        return alumnos;
     }
 }
